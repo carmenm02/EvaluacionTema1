9ebea2c9fef974d92edb18991586f5e15fb44309
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-09-23 16:45:05 +0200

    he creado los for

--- ejercicio4.py
+++ ejercicio4.py
@@ -2,6 +2,8 @@
 
 import sys
 
+from pyparsing import col
+
 
 if len(sys.argv) == 3:
     filas = int(sys.argv[1])
@@ -9,3 +11,10 @@ if len(sys.argv) == 3:
 
     if filas < 1 or filas > 9 or  columnas < 1 or columnas > 9:
         print("Error")
+    else:
+        for i in range(filas):
+            print(" ")
+            for c in range(columnas):
+                print(" * ", end='')
+
+    
